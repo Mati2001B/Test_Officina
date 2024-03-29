ad3f437812b88352db03a273a1fdfdbdbbe6322c
rakuja <d.giachetto@studenti.unipi.it>
2024-03-29 13:45:51 +0100

    clean up, add doc

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def get_x_mean_vec() -> list[float]:
+    """
+    Represents the mean value of the x coord for all the segments.
+    A track is approximated to N segments (in our case divided in 21 segments).
+    :return: the list of all the approximated x coordinates, each index refers to the segment (0-> first segment)
+    """
     return [
         -1.,
         -0.208053691275168,
@@ -25,6 +30,12 @@ def get_x_mean_vec() -> list[float]:
 
 
 def get_y_mean_vec() -> list[float]:
+    """
+    Represents the mean value of the y coord for all the segments.
+    A track is approximated to N segments (in our case divided in 21 segments).
+    :return: the list of all the approximated y coordinates, each index refers to the segment (0-> first segment)
+    """
+
     return [
         2.25000000000000,
         2.25000000000000,
@@ -51,13 +62,29 @@ def get_y_mean_vec() -> list[float]:
 
 
 def get_track_width() -> float:
+    """
+    Get the track width (divide it by 2 if you need the distance from the center to outer)
+    :return: track width
+    """
     return 0.8 * (47.8659 - 41.8691)
 
 
+def example_out_fun() -> list[tuple[float, float]]:
+    return [(10., 10.) for _ in range(0, 21)]
+    # same as
+    # return [(10., 10.)] * 20
+    # it creates a list inserting in a loop (0->19) the same element (10.,10.)
+    # the underscore is used to define a variable that will not be used
+
+
 if __name__ == "__main__":
-    pass  # no operation, used to say that the fun does nothing. remove it!
-    # Calculates the outer and inner boundaries.
-    # use the provided functions to get the data
-    # possibly create a new function that does the
+    # You need to calculate the outer and inner boundaries.
+    # use the provided functions to get the data.
+    # create a new function that does the
     # calculation and then return the result (without side effect)
+    # so the desired output would be having a list of (x,y) coord for inner boundary
+    # and (x,y) coord for outer boundary (each with 20 elements, since we have 20 segments)
+    # (x,y) is a tuple => list[tuple[float]]
     # GL HF
+    pass  # no operation, used to say that the fun does nothing. remove it!
+
